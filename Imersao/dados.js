2f799dd6074a9155fe98f2a54b7363d637317d4a
Maria Vitória <153694001+airotivmaria@users.noreply.github.com>
2024-09-06 11:30:31 -0300

    Update dados.js

--- Imersao/dados.js
+++ Imersao/dados.js
@@ -1,15 +1,18 @@
 let dados = [{
     titulo : "O que é",
     descricao : "O jiu-jitsu é uma arte marcial japonesa que se destaca pela utilização de técnicas de alavancagem e controle para subjugar um oponente, mesmo que este seja fisicamente mais forte. Ao invés de depender da força bruta, o jiu-jitsu valoriza a técnica, o conhecimento do corpo humano e a adaptação às diferentes situações de combate.",
-    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu"
+    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu",
+    tags: "jiu jitsu jiu luta"
     },
     {
     titulo : "Quais os benefícios de praticar Jiu-Jitsu?",
     descricao : "O Jiu-Jitsu oferece diversos benefícios para a saúde física e mental, como: aumento da força e flexibilidade, melhora da coordenação motora, redução do estresse, desenvolvimento da autoconfiança e autodisciplina. Além disso, é uma excelente forma de defesa pessoal.",
-    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu"
+    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu",
+    tags: "saude saúde força disciplina defesa pessoal"
     },
     {
     titulo : "Como começar a praticar Jiu-Jitsu?",
     descricao : "Para começar a praticar Jiu-Jitsu, basta encontrar uma academia próxima e procurar um professor qualificado. Não é necessário ter experiência prévia em artes marciais. O mais importante é ter disposição para aprender e se dedicar aos treinos.",
-    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu"
+    link :"https://pt.wikipedia.org/wiki/Jiu-j%C3%ADtsu",
+    tags: "kimono faixa grau ct academia tatame"
     }];
