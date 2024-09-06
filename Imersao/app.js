1d62399c080df1f8ebc9027e949c513bc5237409
Maria Vitória <153694001+airotivmaria@users.noreply.github.com>
2024-09-06 11:30:48 -0300

    Update app.js

--- Imersao/app.js
+++ Imersao/app.js
@@ -3,17 +3,39 @@ function pesquisar(){
     let section = document.getElementById("resultados-pesquisa");
     let resultados = "";//inicializa uma string vazia para armazenar resultados
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+    let titulo = "";
+    let descricao ="";
+    let tags = "";
+
+    //verifica se o valor do campo de pesquisa está vazio - feedback para o usuario
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Digite algo para a pesquisa.</p>"
+        return
+    }
+
     //intera sobre cada dado da lista de dados
     for (let dado of dados){
-        //cria um novo elemento HTML para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if(titulo.includes(campoPesquisa) || (descricao.includes(campoPesquisa)) || (tags.includes(campoPesquisa))){
+            //cria um novo elemento HTML para cada resultado
+            resultados += `
             <div class="item-resultado">
                 <h2>${dado.titulo}</h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
-        `;
-    }
+            `
+        }
+    };
     //atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
+    // verifica se tem resultados, em caso de não é exibido essa mensagem - feedback para o usuario
+    if (!resultados) {
+        section.innerHTML = "<p>Informação não encontrada.</p>"
+        return
+
+    };
 }
